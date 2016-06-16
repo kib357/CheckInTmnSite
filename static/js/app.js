aa5da271f7db630f4cbe08a6d81d075b3b41558c
kib357 <kib357@gmail.com>
2016-06-16 12:02:18 +0500

    Nav - v0.1

--- static/js/app.js
+++ static/js/app.js
@@ -1,28 +1,75 @@
-var ticking = false, minScrolledPosition = 170, navbar = document.getElementById("navbar");
-window.addEventListener('scroll', function (e) {
-    last_known_scroll_position = window.scrollY;
-    if (!ticking) {
-        window.requestAnimationFrame(function () {
-            checkScrolledClass(last_known_scroll_position);
-            ticking = false;
-        });
-    }
-    ticking = true;
-});
-
-function checkScrolledClass(scrollPosition) {
-    if (scrollPosition > minScrolledPosition) {
-        navbar.classList.add("shrink");
-    } else {
-        if (!navbar.classList.contains("no-expand")) {
-            navbar.classList.remove("shrink");
-        }
-    }
-}
-
 document.addEventListener("DOMContentLoaded", function (event) {
+    initNav();
     initLightbox();
 });
+
+function initNav() {
+    var ticking = false, minScrolledPosition = 170, navbar = document.getElementById("navbar");
+    window.addEventListener('scroll', updateScrollPosition);
+    setInterval(updateScrollPosition, 200);
+
+    var navLinks = document.querySelectorAll("[data-active-until]"), navLinkEndElements = [], navLinkStartElements = [];
+    for (var i = 0; i < navLinks.length; i++) {
+        var endId = navLinks[i].getAttribute("data-active-until");
+        var endEl = document.getElementById(endId);
+        navLinkEndElements.push(endEl || null);
+        var startId = navLinks[i].getAttribute("data-active-after");
+        var startEl = document.getElementById(startId);
+        navLinkStartElements.push(startEl || null);
+    }
+
+    function updateScrollPosition() {
+        last_known_scroll_position = window.scrollY;
+        if (!ticking) {
+            window.requestAnimationFrame(function () {
+                checkScrolledClass(last_known_scroll_position);
+                if (navbar.className.indexOf("blog") < 0) {
+                    updateActiveLink();
+                }
+                ticking = false;
+            });
+        }
+        ticking = true;
+    }
+
+    function checkScrolledClass(scrollPosition) {
+        if (scrollPosition > minScrolledPosition) {
+            if (navbar.className.indexOf("shrink") < 0) {
+                navbar.classList.add("shrink");
+            }
+        } else {
+            if (navbar.className.indexOf("shrink") >= 0 && navbar.className.indexOf("no-expand") < 0) {
+                navbar.classList.remove("shrink");
+            }
+        }
+    }
+
+    function updateActiveLink() {
+        var found = false;
+        for (var i = 0; i < navLinks.length; i++) {
+            if (!found && isLinkActive(navLinkEndElements[i], navLinkStartElements[i])) {
+                found = true;
+                navLinks[i].classList.add("active");
+            } else {
+                navLinks[i].classList.remove("active");
+            }
+        }
+    }
+
+    function isLinkActive(endEl, startEl) {
+        if (endEl == null) {
+            return false;
+        }
+        if (startEl) {
+            var sRect = startEl.getBoundingClientRect();
+            if (sRect.top > (window.innerHeight || document.documentElement.clientHeight)) {
+                return false;
+            }
+        }
+        var rect = endEl.getBoundingClientRect();
+        return rect.top >= (window.innerHeight || document.documentElement.clientHeight) / 2;
+    }
+}
 
 function initLightbox() {
     var imgGroups = {}, currentGroup = [], currentUrl = "", lb, lbClose, lbBack, lbImgWrapper, lbImg;
@@ -84,5 +131,28 @@ function initLightbox() {
         }
         currentUrl = currentGroup[nextIndex];
         lbImg.setAttribute("src", currentUrl);
+    }
+}
+
+function isElementInViewport(el) {
+    if (typeof el === "string") {
+        el = document.getElementById(el);
+    }
+    if (el == null) {
+        return false;
+    }
+    var rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+    );
+}
+
+function scrollIntoView(eleID) {
+    var e = document.getElementById(eleID);
+    if (!!e && e.scrollIntoView) {
+        e.scrollIntoView();
     }
 }
