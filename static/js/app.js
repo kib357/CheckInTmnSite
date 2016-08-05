88f1946b1525acb306934b89fdcb18e14b9ac74f
kib357 <kib357@gmail.com>
2016-08-05 16:45:52 +0500

    https fix

--- static/js/app.js
+++ static/js/app.js
@@ -458,7 +458,7 @@ function sendOrder(data, cb) {
         }
     });
 
-    xhr.open('POST', 'http://api.checkintmn.ru/hooks/orders/make-order');
+    xhr.open('POST', 'https://api.checkintmn.ru/hooks/orders/make-order');
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhr.send("data=" + JSON.stringify(data));
 }
